c0a99648216e9c933511b7c2b8f85b35ea57b848
leandrosrlacerda <leandrolacerda.sistema@gmail.com>
2022-09-10 22:54:40 -0300

    Primeiro commit

---
+++ curso_programacao/src/curso_programacao/Main.java
@@ -0,0 +1,24 @@
+package curso_programacao;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Entre com a quantidade de minutos consumidos no plano:");
+		int minutos = scanner.nextInt();
+		
+		double conta = 50.0;
+		
+		if (minutos > 100) {
+			conta = conta + (minutos - 100) * 2;
+		}
+		
+		System.out.printf("Valor da conta: R$%.2f\n", conta);
+		
+		scanner.close();
+	}
+}
